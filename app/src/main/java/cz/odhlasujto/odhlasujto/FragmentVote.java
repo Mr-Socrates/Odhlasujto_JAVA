4de91fb8ebe0e061da35b6192b59272d11a8d181
MARTIN KOZÁK <kozak_martin@yahoo.com>
2014-12-01 17:09:39 +0100

    createPoll

---
+++ app/src/main/java/cz/odhlasujto/odhlasujto/FragmentVote.java
@@ -0,0 +1,21 @@
+package cz.odhlasujto.odhlasujto;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by martin on 01.12.14.
+ */
+public class FragmentVote extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_poll, container, false);
+
+        return view;
+    }
+}
